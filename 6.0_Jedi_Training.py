3363995f3bb91dac2550430a4afed0a087651b3d
bakerkai <bakerkai@urbandaleschools.com>
2021-10-28 10:03:51 -0500

    yes

--- 6.0_Jedi_Training.py
+++ 6.0_Jedi_Training.py
@@ -3,19 +3,19 @@ Sign your name:_______Kai___
 
 
 1.) Convert Binary 101010 to Hexadecimal.
-
+    42
 2.) Convert Binary 11101001 to Octal.
-
+    351
 3.) Convert Hexadecimal FC to Binary.
-
+    11111100
 4.) Convert Hexadecimal 1F to Decimal.
-
+    31
 5.) Convert Hexadecimal #FAAFBD to RGB.
-
+    250 175 79
 6.) Convert Octal 70 to Binary.
-
+1110000
 7.) Convert RGB (32,128,64) to Hexadecimal.
-
+#208040
 8.) Convert 01000001, 01010011, 01000011, 01001001, 01001001 to ASCII.
-
+    ASCII
 '''
